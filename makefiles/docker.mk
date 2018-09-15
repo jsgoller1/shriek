b6a2b003af68aa8c1f48c03a99e497817bac2a61
Joshua Goller <joshua.goller@uber.com>
2018-09-14 23:41:48 -0700

    improve docker makefile

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -1,19 +1,27 @@
+CONTAINER_NAME:=shriek
+BASE_IMAGE:=ubuntu
+
 ### Dockerized Linux workspace for consistent environment
 docker-clean:
-	-docker stop ubuntu
-	-docker rm ubuntu
+	-docker stop $(CONTAINER_NAME)
+	-docker rm $(CONTAINER_NAME)
 
-docker: docker-clean
-	docker pull ubuntu
+docker-pull:
+	docker pull $(BASE_IMAGE)
+
+container:
 	docker run \
 	-dt \
-	--name ubuntu \
-	-v `pwd`:/shriek \
-	ubuntu
-	docker exec ubuntu apt-get update
-	docker exec ubuntu apt-get install -y make valgrind clang clang-tools cdecl perl bsdmainutils
+	--name $(CONTAINER_NAME) \
+	-p 6666:6666 \
+	-v `pwd`:/$(CONTAINER_NAME) \
+	$(BASE_IMAGE)
+
+docker-install:
+	docker exec $(CONTAINER_NAME) apt-get update
+	docker exec $(CONTAINER_NAME) apt-get install -y make valgrind clang clang-tools cdecl perl bsdmainutils netcat
 
 shell:
-	docker exec -it ubuntu /bin/bash
+	docker exec -it $(CONTAINER_NAME) /bin/bash
 
-workspace: docker-clean docker shell
+workspace: docker-clean docker-pull container docker-install shell
